b14af25a894f2a6d9abc8f3e706e095d21922b6e
71455 <71455@localhost>
2026-08-20 15:31:03 +0800

    Update common.dart option logic and hbb_common submodule to 78a4555

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -1640,14 +1640,11 @@ String bool2option(String option, bool b) {
   String res;
   if (option.startsWith('enable-') &&
       option != kOptionEnableUdpPunch &&
-      option != kOptionEnableIpv6Punch &&
-      option != kOptionOpenNewConnInTabs &&
-      option != kOptionEnableCheckUpdate) {
+      option != kOptionEnableIpv6Punch) {
     res = b ? defaultOptionYes : 'N';
-  } else if ((option.startsWith('allow-') &&
-              option != kOptionD3DRender &&
-              option != kOptionAllowRemoteConfigModification) ||
+  } else if (option.startsWith('allow-') ||
       option == kOptionStopService ||
+      option == kOptionDirectServer ||
       option == kOptionForceAlwaysRelay) {
     res = b ? 'Y' : defaultOptionNo;
   } else {

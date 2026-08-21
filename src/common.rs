8bfbccd685d47bc6e3060c423036de1f30bb3ef2
71455 <71455@localhost>
2026-08-21 21:23:35 +0800

    custom: api server use http with port instead of https

--- src/common.rs
+++ src/common.rs
@@ -1077,7 +1077,10 @@ fn get_api_server_(api: String, custom: String) -> String {
     if !s0.is_empty() {
         let s = crate::increase_port(&s0, -2);
         if s == s0 {
-           return format!("https://{}", s);
+            return format!("http://{}:{}", s, config::RENDEZVOUS_PORT - 2);
+        } else {
+            return format!("http://{}", s);
+        }
     }
     "https://admin.rustdesk.com".to_owned()
 }

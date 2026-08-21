bc213b4037f79d8848557764627a4ae57114f6f7
71455 <71455@localhost>
2026-08-21 19:26:23 +0800

    use https for api server

--- src/common.rs
+++ src/common.rs
@@ -1077,9 +1077,9 @@ fn get_api_server_(api: String, custom: String) -> String {
     if !s0.is_empty() {
         let s = crate::increase_port(&s0, -2);
         if s == s0 {
-            return format!("http://{}:{}", s, config::RENDEZVOUS_PORT - 2);
+            return format!("https://{}", s);
         } else {
-            return format!("http://{}", s);
+            return format!("https://{}", s);
         }
     }
     "https://admin.rustdesk.com".to_owned()
